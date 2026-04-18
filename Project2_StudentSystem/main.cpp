bcdf97be1ac55b90c5d93dbabbb8b02b8ba97b83
zac <abderrazaklahcene90@gmail.com>
2026-04-18 17:05:52 +0100

    push changes

--- Project2_StudentSystem/main.cpp
+++ Project2_StudentSystem/main.cpp
@@ -1,10 +1,60 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main () {
     
     // write code here
-    cout << "Hello World\n";
+   
+    
+    string students [100];
+    double scores[100];
+    int menu;
+    int countStudent = 0;
+
+    // cin >> students[100];
+    // cin >> scores;
+    
+    while (true) {
+    cout << "\n=====Menu====\n";
+    cout << "1- Add student\n";
+    cout << "2- Show all students\n";
+    cout << "3- Find highest score\n";
+    cout << "4- Find average score\n";
+    cout << "5- Search student by name\n";
+    cout << "6- Exit\n";
+    
+
+    cin >> menu;
+
+    switch (menu) {
+        case 1:
+        cout << "Enter Student Name And Score\n";
+        cin >> students[countStudent];
+        cin >> scores[countStudent];
+        countStudent++;
+        break;
+        case 2:
+        for (int i = 0; i < countStudent; i++) {
+            cout << students[i] <<  "Score:" << scores[i] << endl;
+            cout << "=== END ===\n";
+        }
+        break;
+        // case 3:
+
+
+    }
+
+
+
+
+    } 
+
+
+
+
+
+
 
     return 0;
 }
